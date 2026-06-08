bcb2f984b17ae1d87a7abd59a1d0d675b2117c73
agrajjain4786 <agrajjain4786@gmail.com>
2026-06-08 16:53:10 +0530

    first commit

---
+++ sequencerunnable.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+# 1. Prompt Template
+prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in simple words."
+)
+
+# 2. Model
+model = ChatMistralAI(model = 'mistral-small-2506')
+
+# 3. Output parser
+parser = StrOutputParser()
+
+# Step-by-Step manual flow
+
+# Format the prompt
+formatted_prompt = prompt.format_messages(topic = 'Machine Learning')
+
+# Call the model
+responce = model.invoke(formatted_prompt)
+
+# Parse the Output manually
+final_output = parser.parse(responce.content)
+
+print(final_output)
