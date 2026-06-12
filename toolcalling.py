4b63222b119f2026d93cbf746114331a8febacd9
agrajjain4786 <agrajjain4786@gmail.com>
2026-06-12 14:40:45 +0530

    add tool for calculating text length and integrate with ChatMistralAI

---
+++ toolcalling.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_mistralai import ChatMistralAI
+from langchain.tools import tool
+
+from rich import print
+
+# 1. crating a tool
+@tool
+def get_text_length(text : str) -> int:
+    """Returns the number of character in a given text"""
+    return len(text)
+
+model = ChatMistralAI(model = 'mistral-small-2506')
+
+# 2. tool binding
+model_with_tool = model.bind_tools([get_text_length])
+
+result = model.invoke("hello")
+
+result2 = model_with_tool.invoke("hello")
+print(result)
+print()
+print()
+print()
+print(result2)
