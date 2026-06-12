79095effe0f10d42c265f278eb6d1232ace75e74
agrajjain4786 <agrajjain4786@gmail.com>
2026-06-12 17:25:48 +0530

    fix: update invoke calls to use descriptive text for clarity

--- toolcalling.py
+++ toolcalling.py
@@ -16,9 +16,9 @@ model = ChatMistralAI(model = 'mistral-small-2506')
 # 2. tool binding
 model_with_tool = model.bind_tools([get_text_length])
 
-result = model.invoke("hello")
+result = model.invoke("Returns the number of character in a given text : 'hello how are you'")
 
-result2 = model_with_tool.invoke("hello")
+result2 = model_with_tool.invoke("Returns the number of character in a given text : 'hello how are you'")
 print(result)
 print()
 print()
